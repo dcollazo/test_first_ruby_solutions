8175a51d8a9e32b65e35cbcba0e97bb566f8558a
dcollazo <davidcollazo@gmail.com>
2013-04-26 21:31:01 -0700

    Finished 14_array_extensions

--- 14_array_extensions/array_extensions_spec.rb
+++ 14_array_extensions/array_extensions_spec.rb
@@ -1,12 +1,4 @@
-# # Array Extension
-#
-# # Topics
-#
-# * objects, methods, classes
-# * reopening classes
-#
-
-require "array_extensions" # we don't call it "array.rb" since that would be confusing
+require "array_extensions"
 
 describe Array do
 
@@ -24,22 +16,23 @@ describe Array do
     end
   end
 
-  describe '#square' do
-    it "does nothing to an empty array" do
-      [].square.should == []
+    describe '#square' do
+      it "does nothing to an empty array" do
+        [].square.should == []
+      end
+
+      it "returns a new array containing the squares of each element" do
+        [1,2,3].square.should == [1,4,9]
+      end
     end
 
-    it "returns a new array containing the squares of each element" do
-      [1,2,3].square.should == [1,4,9]
+    describe '#square!' do
+      it "squares each element of the original array" do
+        array = [1,2,3]
+        array.square!
+        array.should == [1, 4, 9]
+      end
     end
-  end
-
-  describe '#square!' do
-    it "squares each element of the original array" do
-      array = [1,2,3]
-      array.square!
-      array.should == [1,4,9]
-    end
-  end
-
 end
+
+
